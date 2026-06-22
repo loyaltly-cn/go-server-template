d541867e7fe3959ce64130e9f0977c08f9981d2f
loyal <loyaltly.cn@gmail.com>
2026-06-22 17:24:57 +0800

    chore: update gitignore and untrack .env

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -1,7 +1,6 @@
 package auth
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -61,7 +60,6 @@ func (h *Handler) Login(
 func (h *Handler) Me(c *gin.Context) {
 
 	userIDVal, exists := c.Get("user_id")
-	fmt.Print(exists)
 	if !exists {
 		c.JSON(401, gin.H{"error": "unauthorized"})
 		return
@@ -69,13 +67,20 @@ func (h *Handler) Me(c *gin.Context) {
 
 	userID := userIDVal.(int64)
 
-	user, err := h.service.GetMe(userID)
+	u, err := h.service.GetMe(userID)
 	if err != nil {
 		c.JSON(404, gin.H{"error": "user not found"})
 		return
 	}
 
+	resp := MpAuthResp{
+		ID:     u.ID,
+		Name:   u.Name,
+		Avatar: u.Avatar,
+		Role:   u.Role,
+	}
+
 	c.JSON(200, gin.H{
-		"data": user,
+		"data": resp,
 	})
 }
